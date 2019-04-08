2f2374b24adf3d9ca9bd0e70b032ca3239fc3e5c
Bruno Giannotti <bruno.mayol.giannotti@gmail.com>
2019-04-07 22:05:12 -0300

    Register route completed

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 const express = require('express'),
 app = express();
+const bodyParser = require('body-parser');
+
+// Bodyparsers middleware
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
 
 const users = require('./routes/api/user');
 const posts = require('./routes/api/post');
 const profiles = require('./routes/api/profile');
 
+// MongoDB settings and connection
 const mongoose = require("mongoose");
 mongoose.connect("mongodb://localhost/reactApp")
     .then(() => console.log('MongoDB is up and running...'))
@@ -12,6 +18,7 @@ mongoose.connect("mongodb://localhost/reactApp")
 
 const port = process.env.PORT || 3000;
 
+//Routes 
 app.get('/', (req, res) => {
     res.send('Working');
 });
--- routes/api/user.js
+++ routes/api/user.js
@@ -1,5 +1,10 @@
 const express = require('express');
 const router = express.Router();
+const gravatar = require('gravatar');
+const bcrypt = require('bcryptjs');
+
+// Load user Model
+const UserModel = require('../../models/User');
 
 // @route GET api/user/test
 // @desc Tests user route
@@ -7,6 +12,43 @@ const router = express.Router();
 
 router.get('/test', (req, res) => {
     res.json({msg: "Users works!"});
+});
+
+// @route POST api/user/register
+// @desc Register a user
+// @access Public
+
+router.post('/register', (req, res) => {
+    UserModel.findOne({ email: req.body.email })
+    .then((user) => {
+        if (user) {
+            return res.status(400).json({email: 'Email already in use.'});
+        } else {
+            const avatar = gravatar.url(req.body.email, {
+                s: '200', // size
+                r: 'pg', // rating
+                d: 'mm' // default
+            });
+            const user_object = {
+                name: req.body.name,
+                email: req.body.email,
+                avatar: avatar,
+                password: req.body.password
+            }
+            const newUser = new UserModel(user_object);
+            bcrypt.genSalt(10, (err, salt) => {
+                bcrypt.hash(newUser.password, salt, (ex, hash) => {
+                    if (ex) throw ex;
+                    newUser.password = hash;
+                    newUser.save()
+                    .then((user) => {
+                        res.json(user)
+                    })
+                    .catch((error) => console.log(error));
+                });
+            });
+        }
+    });
 });
 
 module.exports = router;
